edbafd8f1f75ac9178daaefef9aca87d2e41986b
Som Shekhar <iamssmkhrj@gmail.com>
2024-05-31 18:56:52 +0530

    Fix CSS

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import NovaImg from "./assets/Nova.jpeg";
 function CardWithCSS({ img, name, company, talkTitle }) {
   return (
     <div className="w-96 space-y-4">
-      <div className="relative rounded-2xl overflow-hidden group`">
+      <div className="relative rounded-2xl overflow-hidden group">
         <img src={img} alt={name} className="grayscale" />
         <div className="absolute inset-0 hidden group-hover:flex flex-col bg-orange-400 p-8 justify-end gap-1 backdrop-blur-md">
           <p>Talk Title</p>
